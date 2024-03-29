0e91e1a9ad53303ebb7a350e9aaf9526c29bba3c
timothy-d-lambert <tdlambert23@gmail.com>
2024-03-29 14:18:50 -0400

    Rendered the book.

--- build_book.R
+++ build_book.R
@@ -4,7 +4,7 @@ library(knitr)
 # Render all
 quarto::quarto_render(output_format = "html")
 
-# Render 'chapt_name' chpater only
+# Render 'chapt_name' chapter only
 quarto::quarto_render("chapt_name.qmd", 
                       #cache_refresh = TRUE, # default is FALSE
                       output_format = "html")
